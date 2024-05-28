2a2c61e9d5ad2c20b29cee000839bf27414ab216
jaeyoung <pjyng115@naver.com>
2024-05-28 23:17:02 +0900

    First commit

---
+++ django-coplate/coplate_project/coplate/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom fields', {'fields': ('nickname',)}),  # Add your custom field here
+    )
+admin.site.register(User, UserAdmin)
